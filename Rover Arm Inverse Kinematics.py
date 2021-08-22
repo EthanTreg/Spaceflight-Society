87a65cbc30f8e2739c49bb48f0e4d4fd209b85f5
EthanTreg <56436083+EthanTreg@users.noreply.github.com>
2021-08-22 13:20:05 +0100

    Improved robustness of inverse kinematics
    Fixed arm not favouring closest solution.
    Now calculates all possible solutions and finds the best solution that is correct.
    Fixed error when target position is to close to the center.

--- Rover Arm Inverse Kinematics.py
+++ Rover Arm Inverse Kinematics.py
@@ -14,22 +14,17 @@ def main():
 
     #Constants
     upper_length_m = 0.1
-    lower_length_m = 0.1
+    lower_length_m = 0.2
     upper_angle_limit_rad = [-np.pi, 0] #Not implimented yet
     lower_angle_limit_rad = [0, np.pi / 2] #Not implimented yet
     lower_angle_rad = 0 #Initial angle - Not implimented yet
     upper_angle_rad = np.pi / 2 #Initial angle - Not implimented yet
-    y_solutions = [0, 0]
+    y_solutions = np.array((0., 0.))
     joint_position_m = [0, lower_length_m]
-    head_position_m = head_target_position_m = np.array((upper_length_m, lower_length_m))
+    head_target_position_m = np.array((upper_length_m, lower_length_m))
     arm_square_difference_m = lower_length_m ** 2 - upper_length_m ** 2
 
-    #Arm visulisation
-    plt.figure()
-    plt.plot((0, joint_position_m[0], head_target_position_m[0]), (0, joint_position_m[1], head_target_position_m[1]))
-    plt.xlim(0, upper_length_m + lower_length_m)
-    plt.ylim(0, upper_length_m + lower_length_m)
-    plt.show(block=False)
+    plot_arm(joint_position_m, head_target_position_m, upper_length_m, lower_length_m)
 
     while True:
         direction = input()
@@ -46,43 +41,54 @@ def main():
         elif direction.lower() == 'q':
             head_target_position_m[1] -= 0.01
 
-
         head_target_distance_m = np.sqrt(np.sum(head_target_position_m ** 2))
 
-        #If the head target location is out of reach, set the coordinates to the maximum range in same direction
+        #If the head target location is out of reach, set the coordinates to the maximum/minimum range in same direction
         if head_target_distance_m > upper_length_m + lower_length_m:
             head_target_position_m *= (upper_length_m + lower_length_m) / head_target_distance_m
             head_target_distance_m = upper_length_m + lower_length_m
+        elif head_target_distance_m < lower_length_m - upper_length_m:
+            head_target_position_m *= (lower_length_m - upper_length_m) / head_target_distance_m
+            head_target_distance_m = lower_length_m - upper_length_m
 
-        #Calculate first y solution
-        y_solutions[0] = y_solutions[1] = 0.5 * head_target_position_m[1] * (1 + arm_square_difference_m / (head_target_distance_m ** 2))
-        y_solutions[0] += np.sqrt(abs(
+        #Calculate y solutions
+        y_weighted_mid_point_m = 0.5 * head_target_position_m[1] * (1 + arm_square_difference_m / (head_target_distance_m ** 2))
+        y_mid_point_offset_m = np.sqrt(abs(
             - head_target_distance_m ** 6 + head_target_distance_m ** 4 * (head_target_position_m[1] ** 2 - 2 * arm_square_difference_m)
             + head_target_distance_m ** 2 * (2 * head_target_position_m[1] ** 2 * arm_square_difference_m - arm_square_difference_m ** 2 + 4 * head_target_position_m[0] ** 2 * lower_length_m ** 2)
             + head_target_position_m[1] ** 2 * arm_square_difference_m ** 2)) / (2 * head_target_distance_m ** 2)
 
-        #Calculate second y solution
-        y_solutions[1] -= np.sqrt(abs(
-            - head_target_distance_m ** 6 + head_target_distance_m ** 4 * (head_target_position_m[1] ** 2 - 2 * arm_square_difference_m)
-            + head_target_distance_m ** 2 * (2 * head_target_position_m[1] ** 2 * arm_square_difference_m - arm_square_difference_m ** 2 + 4 * head_target_position_m[0] ** 2 * lower_length_m ** 2)
-            + head_target_position_m[1] ** 2 * arm_square_difference_m ** 2)) / (2 * head_target_distance_m ** 2)
+        y_solutions[0] = y_weighted_mid_point_m + y_mid_point_offset_m
+        y_solutions[1] = y_weighted_mid_point_m - y_mid_point_offset_m
 
-        #Take the y solution colsest to the current y coordinate
-        if abs(y_solutions[0] - joint_position_m[1]) < abs(y_solutions[1] - joint_position_m[1]):
-            joint_position_m[1] = y_solutions[0]
-            joint_position_m[0] = np.sqrt(lower_length_m ** 2 - y_solutions[0] ** 2)
+        #Calculate correct x & y solutions
+        x_solutions = np.sqrt(np.where((lower_length_m ** 2 - y_solutions ** 2 < 0) & (abs(lower_length_m ** 2 - y_solutions ** 2) < 1e-3), 0, lower_length_m ** 2 - y_solutions ** 2))
+        x_solutions = np.vstack((x_solutions, -x_solutions))
+        solution_arm_lengths_m = np.sqrt((head_target_position_m[1] - y_solutions) ** 2 + (head_target_position_m[0] - x_solutions) ** 2)
+        solution_idx = np.argwhere(abs(solution_arm_lengths_m - upper_length_m) < 1e-4)
+        solutions = np.transpose(np.vstack((x_solutions[solution_idx[:, 0], solution_idx[:, 1]], y_solutions[solution_idx[:, 1]])))
 
-        else:
-            joint_position_m[1] = y_solutions[1]
-            joint_position_m[0] = np.sqrt(lower_length_m ** 2 - y_solutions[1] ** 2)
+        #Find correct solution closest to current position
+        solution_distance_m = np.sqrt((joint_position_m[0] - solutions[:, 0]) ** 2 + (joint_position_m[1] - solutions[:, 1]) ** 2)
+        joint_position_m = solutions[np.argmin(solution_distance_m)]
 
-        #Plots new arm
-        plt.clf()
-        plt.plot((0, joint_position_m[0], head_target_position_m[0]), (0, joint_position_m[1], head_target_position_m[1]))
-        plt.xlim(0, upper_length_m + lower_length_m)
-        plt.ylim(0, upper_length_m + lower_length_m)
-        plt.show(block=False)
+        plot_arm(joint_position_m, head_target_position_m, upper_length_m, lower_length_m)
 
-    return
+def plot_arm(joint_position_m, head_target_position_m, upper_length_m, lower_length_m):
+    """
+    Arm visualisation.
+    ### Params:
+        joint_position_m: x, y position of elbow pivot
+        head_target_position_m: x, y position of claw
+        upper_length_m: length of forearm
+        lower_length_m: length of aftarm
+    """
+    plt.clf()
+    ax = plt.gca()
+    plt.plot((0, joint_position_m[0], head_target_position_m[0]), (0, joint_position_m[1], head_target_position_m[1]))
+    plt.xlim(-upper_length_m - lower_length_m, upper_length_m + lower_length_m)
+    plt.ylim(-upper_length_m - lower_length_m, upper_length_m + lower_length_m)
+    ax.set_aspect('equal')
+    plt.show(block=False)
 
 main()
